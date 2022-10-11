dd06c58862418159074ef13eef02d48398b802f3
Susana Reyes <60724497+susi1603@users.noreply.github.com>
2022-10-11 09:21:27 +0200

    update rotation matrices

--- src/visp_ros/tutorial/franka/real-robot/tutorial-franka-real-pbvs-apriltag.cpp
+++ src/visp_ros/tutorial/franka/real-robot/tutorial-franka-real-pbvs-apriltag.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef4fb0c35e69be117650dd9083cf1346f71adaf86612e561c3330d04e4170e01
-size 13748
+oid sha256:2f621014832c6ce1ceacad41fc81268db010b085eea8e23a955a69ea85886af7
+size 14091
