1205e763859cdf2def307a76e5db72fa7893211a
Susana Reyes <60724497+susi1603@users.noreply.github.com>
2022-10-21 16:51:28 +0200

    update

---
+++ src/visp_ros/tutorial/franka/coppeliasim/original.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e59d99692f452adaba38a308cfb2c3aeec6681b45e75c7e3f6606aed770bad99
+size 16021
--- src/visp_ros/tutorial/franka/coppeliasim/tutorial-franka-coppeliasim-pbvs-apriltag.cpp
+++ src/visp_ros/tutorial/franka/coppeliasim/tutorial-franka-coppeliasim-pbvs-apriltag.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05197566490021738ec609ebc5d660343d6a0aac4341026172ebca592c499513
-size 10341
+oid sha256:d40c9c9ce40efbc3414657a6c77cb103e8f515e2e73ee32eb358164dcd17b73e
+size 11307
